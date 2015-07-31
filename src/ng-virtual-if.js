11be65c594204e8ad56e90fc802cd8a99b8dda9b
Marcio Junior <marciojunior_eu@yahoo.com.br>
2015-07-31 08:33:34 -0300

    Fix identation

--- src/ng-virtual-if.js
+++ src/ng-virtual-if.js
@@ -1,10 +1,10 @@
 (function (root, factory) {
-    if (typeof define === 'function' && define.amd) {
-        define([], factory);
-    } else if (typeof module === 'object' && module.exports) {
-        module.exports = factory();
-    } else {
-        root.returnExports = factory();
+  if (typeof define === 'function' && define.amd) {
+    define([], factory);
+  } else if (typeof module === 'object' && module.exports) {
+    module.exports = factory();
+  } else {
+    root.returnExports = factory();
   }
 }(this, function () {
 
